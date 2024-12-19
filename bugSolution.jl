f0366708e1ea97840fda811ccfda952c43fa89dc
BugHunter-X <askzenai@gmail.com>
2024-12-19 07:48:06 +0100

    Add bugSolution.jl file

---
+++ bugSolution.jl
@@ -0,0 +1,24 @@
+```julia
+function myfunction(x)
+  if x > 0
+    return x^2
+  else
+    return x^2
+  end
+ end
+
+println(myfunction(2)) # Output: 4
+println(myfunction(-2)) #Output: 4
+
+function myfunction2(x::Number)
+  y = abs(x)^2
+  if x<0
+    return -y
+  else
+    return y
+  end
+ end
+
+println(myfunction2(2)) # Output: 4
+println(myfunction2(-2)) # Output: -4
+```
